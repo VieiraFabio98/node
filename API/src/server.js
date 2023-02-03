6cf909e03386d80aef2adb7e47a7e9b3d5ab9c05
Fabio <vieirafabio5271@outlook.com>
2023-02-03 11:32:52 -0300

    explicação de request params e query

--- API/src/server.js
+++ API/src/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const PORT = 3333;
 const app = express();
 
+//request params são obrigatórios
 app.get("/message/:id/:user", (request, response) => {
   const {id, user} = request.params;
 
   response.send(`id da mensagem: ${id}. Para o usuário : ${user}`);
+});
+
+//request query não é obrigatório
+app.get("/users", (request, response) =>{
+  const {page, limit} = request.query;
+  response.send(`Página = ${page}\n limite = ${limit}`);
 });
 
 app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
