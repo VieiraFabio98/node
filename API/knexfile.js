f6031142afee1e23d4ee24642afb4eb34d9f3c64
Fabio <vieirafabio5271@outlook.com>
2023-02-08 14:37:07 -0300

    criado a tabela de tags e links. Adicionando o delete do sqlite

--- API/knexfile.js
+++ API/knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "dataBase.db")
     },
+    pool:{
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cn)
+    },
     migrations:{
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
